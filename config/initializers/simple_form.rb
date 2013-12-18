d7cc9eec2a7b99f8ddd78702fcafa4a09173aa22
Vadus <bekish94.94@mail.ru>
2013-12-19 02:31:26 +0300

    Fix bootstrap form

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -66,7 +66,7 @@ SimpleForm.setup do |config|
   config.error_notification_tag = :div
 
   # CSS class to add for error notification helper.
-  config.error_notification_class = 'error_notification'
+  config.error_notification_class = 'alert alert-error'
 
   # ID to add for error notification helper.
   # config.error_notification_id = nil
@@ -95,7 +95,7 @@ SimpleForm.setup do |config|
   # config.label_text = lambda { |label, required| "#{required} #{label}" }
 
   # You can define the class to use on all labels. Default is nil.
-  # config.label_class = nil
+  config.label_class = 'control-label'
 
   # You can define the class to use on all forms. Default is simple_form.
   # config.form_class = :simple_form
@@ -111,7 +111,7 @@ SimpleForm.setup do |config|
   # in this configuration, which is recommended due to some quirks from different browsers.
   # To stop SimpleForm from generating the novalidate option, enabling the HTML5 validations,
   # change this configuration to true.
-  config.browser_validations = false
+  config.browser_validations = true
 
   # Collection of methods to detect if a file type was given.
   # config.file_methods = [ :mounted_as, :file?, :public_filename ]
@@ -139,11 +139,4 @@ SimpleForm.setup do |config|
 
   # Cache SimpleForm inputs discovery
   # config.cache_discovery = !Rails.env.development?
-
-  # Default class for inputs
-  # config.input_class = nil
-
-  # Defines if the default input wrapper class should be included in radio
-  # collection wrappers.
-  # config.include_default_input_wrapper_class = true
 end
--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,8 +1,10 @@
 # Use this setup block to configure all options available in SimpleForm.
+# Inside your views, use the 'simple_form_for' with one of the Bootstrap form
+# classes, '.form-horizontal', '.form-inline', '.form-search' or
+#  '.form-vertical', as the following:
+#
+# = simple_form_for(@user, html: {class: 'form-horizontal' }) do |form|'
 SimpleForm.setup do |config|
-  config.error_notification_class = 'alert alert-error'
-  config.label_class = 'control-label'
-
   config.wrappers :bootstrap, tag: 'div', class: 'control-group', error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
@@ -40,8 +42,8 @@ SimpleForm.setup do |config|
     end
   end
 
-  # Wrappers for forms and inputs using the Bootstrap toolkit.
-  # Check the Bootstrap docs (http://getbootstrap.com/2.3.2/)
+  # Wrappers for forms and inputs using the Twitter Bootstrap toolkit.
+  # Check the Bootstrap docs (http://twitter.github.com/bootstrap)
   # to learn about the different styles for forms and inputs,
   # buttons and other elements.
   config.default_wrapper = :bootstrap
